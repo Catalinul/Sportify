cd9bd9cbacff7b2cf18fbd86f373a52acfdedeac
Catalinul <pirvu.catalin15@gmail.com>
2022-04-04 00:47:59 +0300

    added guests screen

--- App.js
+++ App.js
@@ -17,10 +17,11 @@ import {
 import HomeScreen from './src/screens/Home';
 import SearchResultsScreen from './src/screens/SearchResults';
 import DestinationSearchScreen from './src/screens/DestinationSearch';
+import GuestsScreen from './src/screens/Guests';
 
 import Post from './src/components/Post';
-
 import feed from './assets/data/feed'; //date hardcodate pt postari
+
 
 const post1 = feed[0];
 const post2 = feed[1];
@@ -33,13 +34,16 @@ const App: () => Node = () => {
     <StatusBar barStyle="dark-content" />
     <SafeAreaView>
 
-      {/*<HomeScreen></HomeScreen>}
-      <Post post={post1} />
-      <Post post={post2} />*/}
+      {/* <HomeScreen></HomeScreen> */}
+
+      {/* <Post post={post1} /> */}
+      {/* <Post post={post2} /> */}
 
       {/* <SearchResultsScreen/> */}
 
-      <DestinationSearchScreen/>  
+      {/* <DestinationSearchScreen/>   */}
+
+      <GuestsScreen/>
 
     </SafeAreaView>
     </>
---
+++ src/screens/Guests/index.js
@@ -0,0 +1,81 @@
+import React, {useState} from 'react'; //folosesc hook-ul useState pentru a tine evidenta numarului de adulti/copii etc
+import { View, Text, Pressable } from 'react-native';
+import styles from './styles.js'; 
+
+
+const GuestsScreen = (props) => {
+    const [adults, setAdults] = useState(0); // adults - valoarea, setAdults - setter, 0 - valoare initiala 
+    const [childrens, setChildrens] = useState(0); // la fel ca mai sus
+
+    return (
+        <View>
+
+            {/* Row 1: Adults */}
+            <View style = {styles.row} > 
+
+                {/* Titles */}
+                <View> 
+                    <Text style={{fontWeight: 'bold', color: 'black'}}>Adults</Text>
+                    <Text style={{color: '#8d8d8d'}}>Ages 13 or above</Text>
+                </View>
+
+                {/* Buttons with value */}
+                <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                    {/* minus button */}
+                    <Pressable 
+                        onPress = { () => setAdults(Math.max(0, adults - 1)) }
+                        style = {styles.button}>
+                        <Text style={{fontSize: 20, color: 'black'}}> - </Text>
+                    </Pressable>
+
+                    {/* value */}
+                    <Text style= {{ marginHorizontal: 20, fontSize: 20}}> {adults} </Text> 
+                    {/* {adults} vine de la useState initializat la inceput */}
+
+                    {/* plus button */}
+                    <Pressable 
+                        onPress = { () => setAdults(adults + 1)  }
+                        style = {styles.button}>
+                        <Text style={{fontSize: 20, color: 'black'}}> + </Text>
+                    </Pressable>
+                </View>
+
+            </View>
+
+            {/* Row 2: Children */}
+            <View style = {styles.row} > 
+
+                {/* Titles */}
+                <View> 
+                    <Text style={{fontWeight: 'bold', color: 'black'}}>Childrens</Text>
+                    <Text style={{color: '#8d8d8d'}}>Ages 2 - 12</Text>
+                </View>
+
+                {/* Buttons with value */}
+                <View style = {{flexDirection: 'row', alignItems: 'center'}}>
+                    {/* minus button */}
+                    <Pressable 
+                        onPress = { () => setChildrens(Math.max(0, childrens - 1)) }
+                        style = {styles.button}>
+                        <Text style={{fontSize: 20, color: 'black'}}> - </Text>
+                    </Pressable>
+
+                    {/* value */}
+                    <Text style= {{ marginHorizontal: 20, fontSize: 20}}> {childrens} </Text> 
+                    {/* {adults} vine de la useState initializat la inceput */}
+
+                    {/* plus button */}
+                    <Pressable 
+                        onPress = { () => setChildrens(childrens + 1)  }
+                        style = {styles.button}>
+                        <Text style={{fontSize: 20, color: 'black'}}> + </Text>
+                    </Pressable>
+                </View>
+
+            </View>
+
+        </View>
+    );
+};
+ 
+export default GuestsScreen;
