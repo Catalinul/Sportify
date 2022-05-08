ce38f00a81f7b15e0c333dfe52ac2601461b217b
Catalinul <pirvu.catalin15@gmail.com>
2022-05-08 16:32:45 +0300

    map and list sync using references

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { View, Text, FlatList, useWindowDimensions} from 'react-native';
 import MapView from 'react-native-maps'; 
 import CustomMarker from '../../components/CustomMarker';
@@ -12,11 +12,49 @@ const SearchResultsMap = (props) => {
 
     const [selectedPlaceId, setSelectedPlaceId] = useState( );
 
+    const flatlist = useRef();
+    const map = useRef();
+
+    const viewConfig = useRef({itemVisiblePercentThreshold: 70})
+    //sincronizam selectarea locatiilor pe harta cu carouselul de locatii (reverse)
+    const onViewChanged = useRef( ({viewableItems }) => {
+        if (viewableItems.length > 0 ) {
+            const selectedPlace = viewableItems[0].item;
+            setSelectedPlaceId(selectedPlace.id)
+            
+        }
+     })
+
+
     const width = useWindowDimensions().width;
+
+
+    //sincronizam selectarea locatiilor pe harta cu carouselul de locatii
+    useEffect( () => {
+
+        if ( !selectedPlaceId || !flatlist) {
+            return;
+        }
+
+        const index = places.findIndex(place => place.id === selectedPlaceId)
+        flatlist.current.scrollToIndex({index})
+
+        //partea de mai jos e mai zoom in automat pe harta
+        const selectedPlace = places[index];
+        const region = {
+            latitude: selectedPlace.coordinate.latitude,
+            longitude: selectedPlace.coordinate.longitude,
+            latitudeDelta: 0.8,
+            longitudeDelta: 0.8,
+        }
+        map.current.animateToRegion(region);
+
+    },  [selectedPlaceId])
 
     return (
         <View>
                 <MapView
+                ref = {map}
                 style = {{ width: '100%', height: '100%'}}
                 initialRegion={{
                     latitude: 28.3279822,
@@ -41,6 +79,7 @@ const SearchResultsMap = (props) => {
                 <View style = {{position: 'absolute', bottom: 40}}>
 
                         <FlatList
+                        ref = {flatlist }
                         data = {places}
                         renderItem = { ({item}) => <PostCarouselItem post = {item}/>}
                         horizontal
@@ -48,6 +87,8 @@ const SearchResultsMap = (props) => {
                         snapToInterval = {width - 60}
                         snapToAlignment = {"center"}
                         decelerationRate = {"fast"}
+                        viewabilityConfig = { viewConfig.current }
+                        onViewableItemsChanged = { onViewChanged.current }
                         />
 
                 </View>
