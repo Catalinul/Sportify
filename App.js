e64ae2f786d7d09ff1656e59e205a2d7fcb4b5a3
Catalinul <pirvu.catalin15@gmail.com>
2022-05-15 16:20:10 +0300

    ampilify init

--- App.js
+++ App.js
@@ -9,10 +9,13 @@
 import React from 'react';
 import type { Node } from 'react';
 import { StatusBar } from 'react-native';
-
 import 'react-native-gesture-handler';
-
 import Router from './src/navigation/Router';
+
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
+
 
 const App: () => Node = () => {
 
